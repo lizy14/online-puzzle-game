464f1967a82eb1c3d1eac7f0ff4c4807df2a0185
Li Zhaoyang <lizy14@yeah.net>
2015-08-22 22:46:33 +0800

    init. hello google mdl!

---
+++ js/notes.js
@@ -0,0 +1,21 @@
+function setNotesContent(text){
+	document.getElementById('user-note').value=text;
+	if(text)
+		document.getElementById('user-note-holder').classList.add('is-dirty');
+	else
+		document.getElementById('user-note-holder').classList.remove('is-dirty');
+}
+
+function clearNotes(){
+	setNotesContent('');
+	storeNotes();
+}
+
+function restoreNotes(){
+	storedUserNotes=localStorage.getItem('userNotes');
+	setNotesContent(storedUserNotes);
+}
+
+function storeNotes(){
+	localStorage.setItem('userNotes',document.getElementById('user-note').value);
+}
